bc18919a655e9563ced605b6e998e8ca4d39f2c1
negasimichael <negasimichael>
2021-01-20 17:42:25 -0600

    Updated file

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     myDateTime();
-   changeDate();
-myStorage();
-   saveButtons();
+    changeDate();
+    myStorage();
+    saveButtons();
 });
 
 function myDateTime() {
@@ -20,7 +20,7 @@ function changeDate() {
     var currentTime = Number(moment().hour());
     $('.form-control').each(function () {
         var eventTime = parseInt($(this).attr('id'));
-        console.log(currentTime,eventTime)
+         // console.log(currentTime, eventTime)
         if (currentTime > eventTime) {
             $(this).addClass('past')
         } else if (currentTime < eventTime) {
@@ -35,19 +35,18 @@ function myStorage() {
     //creating var called element local storage for every hours input
     for (let i = 0; i < localStorage.length; i++) {
         const element = localStorage.key(i);
-        //console.log(element);
-        if(element){
-            // parse that input as element and assign it the that id 
+        // console.log(element);
+        if (element) {
             $('#' + element).val(localStorage.getItem(element));
         }
     }
-} 
+}
 
 function saveButtons() {
     const saveBtn = $(".saveBtn")
-    saveBtn.each(function(index){
+    saveBtn.each(function (index) {
         // set attribute called data-hour. 
-        $(this).attr('data-hour',index + 9)
+        $(this).attr('data-hour', index + 9)
     })
     saveBtn.on("click", function (e) {
         var value = $(this).siblings(".form-control").val()
@@ -59,20 +58,13 @@ function saveButtons() {
 $("#clearDay").on("click", function () {
     // clear day event if confirming yes.
     if (localStorage.length < 9) {
-        var clear = confirm ("Press OK if you want to clear today's events?")
+        var clear = confirm("Press OK if you want to clear today's events?")
         if (clear) {
             for (let i = 9; i <= 17; i++) {
-            
-                    $('#' + i).val('');
-                
+                $('#' + i).val('');
             }
-                
-            
-            
             localStorage.clear();
         }
-
     }
-
 })
 
